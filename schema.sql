9a31912df7e4a842f46a463fd0bdc082c062b3a2
igor <andrigorr@yandex.ru>
2019-04-25 14:27:03 +0300

    module4-task1_corrected

--- schema.sql
+++ schema.sql
@@ -1,8 +1,8 @@
-CREATE DATABASE yeticave
+CREATE DATABASE yeticave1
     DEFAULT CHARACTER SET utf8
     DEFAULT COLLATE utf8_general_ci;
 
-USE yeticave;
+USE yeticave1;
 
 CREATE TABLE categories (
     id INT AUTO_INCREMENT PRIMARY KEY,
@@ -15,13 +15,14 @@ CREATE TABLE lot (
     category_id INT,
     user_id INT,
     user_id_win INT,
-    dt_add TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     title CHAR(255),
-    description CHAR(255),
-    path CHAR(255),
+    description VARCHAR(1024),
+    path TEXT,
     start_price INT,
-    dt_close DATE,
-    step INT
+    closed_at DATE,
+    step INT,
+    FOREIGN KEY (category_id) REFERENCES categories(id)
 );
 
 CREATE TABLE bet (
@@ -29,24 +30,35 @@ CREATE TABLE bet (
     user_id INT,
     lot_id INT,
     bet_price INT,
-    dt_bet TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (lot_id) REFERENCES lot(id)
 );
 
 CREATE TABLE users (
     id INT AUTO_INCREMENT PRIMARY KEY,
     lot_id INT,
     bet_id INT,
-    dt_add TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     email CHAR(128),
     name CHAR(255),
     password CHAR(64),
     avatar_path CHAR(255),
-    contact TEXT
+    contact TEXT,
+    FOREIGN KEY (lot_id) REFERENCES lot(id),
+    FOREIGN KEY (bet_id) REFERENCES bet(id)
 );
 
-CREATE UNIQUE INDEX email ON users(email);
+ALTER TABLE lot
+ADD FOREIGN KEY (user_id) REFERENCES users(id);
 
-CREATE UNIQUE INDEX pass ON users(password);
+ALTER TABLE lot
+ADD FOREIGN KEY (user_id_win) REFERENCES users(id);
+
+ALTER TABLE bet
+ADD FOREIGN KEY (user_id) REFERENCES users(id);
+
+
+CREATE UNIQUE INDEX email ON users(email);
 
 CREATE INDEX lot_title ON lot(title);
 
